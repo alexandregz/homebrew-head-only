12a81f0cf39543cd6c85e6e2aad6f9d6015e8140
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-01-26 00:57:42 +0100

    s/url/head/, removed sha1 hash and I put code for test into libexec

--- wren.rb
+++ wren.rb
@@ -1,18 +1,17 @@
 class Wren < Formula
   homepage "http://munificent.github.io/wren/"
-  url "https://github.com/munificent/wren/archive/efd161cea278e5cc9d6b404451443cf48d4057a8.tar.gz"
-  sha1 "f8757ad1b3dbe312c2928e00e095007d87b97748"
+  head "https://github.com/munificent/wren.git" 
 
   depends_on "pkg-config" => :build
 
   def install
     system "make"
     bin.install "wren"
-    prefix.install Dir["Makefile", "include", "script", "src", "test"]
+    libexec.install Dir["Makefile", "include", "script", "src", "test"]
   end
 
   test do
-    prefix.cd do
+    libexec.cd do
       system "make", "test"
     end
   end
